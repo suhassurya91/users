0e58b04f0c9eea916c5ef3c94c798813d8f1c932
suhassuryawanshi <suhas.suryawanshi@enyotalearning.com>
2017-10-12 16:13:55 +0530

    ok

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -11,10 +11,18 @@ export class TodoComponent implements OnInit {
   
   localVar1 : any;
   constructor(private http: HttpClient){ }
-      ngOnInit(): void {
-      alert("BC");
+    <!-- CODE TO SHOW TODO LISTS-->
+    ngOnInit(): void {
         this.http.get('http://jsonplaceholder.typicode.com/todos?users=1')
         .subscribe(res => this.localVar1 = console.log(res));
         console.log(this.localVar1);
-        }
+    }
+
+    <!-- CODE TO DELETE TODO LISTS-->
+<!--    deleteTodoById(id: number): this.http.get('http://jsonplaceholder.typicode.com/todos?users=1') {
+        this.localVar1 = this.localVar1
+          .filter(localVar1 => localVar1.id !== id);
+        return this;
+    }-->
+
 }
